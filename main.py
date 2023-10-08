55cbf842e2210136dc997ef2b987abb976dd8dc9
hyeongjin94 <48722080+hyeongjin94@users.noreply.github.com>
2023-10-08 15:36:16 +0900

    Updates 2 on Github Only

--- main.py
+++ main.py
@@ -21,3 +21,6 @@ print("Hello World updates on Github")
 
 #updates in local master branch
 print("Hello World Updates on Local Master Branch")
+
+#Updates on main branch of Github
+print("Hello World updates 2 on Github Only")
